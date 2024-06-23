55bdc42636523956b99230120a4749eb7f5eb306
amanuelgthn <amanuelgthn@gmail.com>
2024-06-23 21:01:08 +0200

    progress

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -7,15 +7,9 @@ import { HousingLocation } from 'src/housing-location';
     standalone: true,
     template: `
   <section>
-    <form>
-      <div>
-        <input type="text" placeholder="filter by city">
-        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M15.7832 14.3911L20 18.6069L18.6069 20L14.3911 15.7832C12.8224 17.0407 10.8713 17.7246 8.86088 17.7218C3.96968 17.7218 0 13.7521 0 8.86088C0 3.96968 3.96968 0 8.86088 0C13.7521 0 17.7218 3.96968 17.7218 8.86088C17.7246 10.8713 17.0407 12.8224 15.7832 14.3911ZM13.8082 13.6605C15.0577 12.3756 15.7555 10.6532 15.7527 8.86088C15.7527 5.05267 12.6681 1.96909 8.86088 1.96909C5.05267 1.96909 1.96909 5.05267 1.96909 8.86088C1.96909 12.6681 5.05267 15.7527 8.86088 15.7527C10.6532 15.7555 12.3756 15.0577 13.6605 13.8082L13.8082 13.6605Z" fill="#575757"/>
-        </svg>
-      </div>
-      <button class="primary" type="button">Search</button>
-    </form>
+  <form class="nosubmit">
+  <input class="nosubmit" type="search" placeholder="The most successful IT companies in 2020">
+</form>
   </section>
   <section class="results">
     <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
@@ -28,10 +22,10 @@ export class HomeComponent {
   housingLocationList: HousingLocation[] = [
     {
       id: 0,
-      name: 'Acme Fresh Start Housing',
+      name: 'Lewis Hamiltons House',
       city: 'Chicago',
       state: 'IL',
-      photo: '/assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
+      photo: '/assets/gimme-crescent-heights-la-72716315.webp',
       availableUnits: 4,
       wifi: true,
       laundry: true
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule,
+  providers: [provideRouter(routes)]
+)
   .catch(err => console.error(err));
